e8d33f80ca5ef701cf1a11873d0ad10858bc9074
minjaekim <minjae2246@gmail.com>
2023-06-07 00:22:24 +0900

    회원가입까지 완료

--- src/main/java/com/example/bookAPI/repository/MemberRepository.java
+++ src/main/java/com/example/bookAPI/repository/MemberRepository.java
@@ -1,13 +1,12 @@
 package com.example.bookAPI.repository;
 
-import com.example.bookAPI.entity.User;
+import com.example.bookAPI.domain.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository  extends JpaRepository<User, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
     @EntityGraph(attributePaths = "roles")
-    Optional<User> findOneWithRolesByEmail(String email);
+    Optional<Member> findOneWithRolesByEmail(String email);
 }
